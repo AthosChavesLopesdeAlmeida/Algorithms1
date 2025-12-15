8830b4cdf890dd9cc1ad7986a96d3da11033bcfd
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-12-15 18:33:16 -0300

    15/12/2025

--- binarySearch.js
+++ binarySearch.js
@@ -3,18 +3,27 @@ Encontra o meio do array
 Se o número buscado for menor que o valor do meio do array, descarta a parte direita e busca denovo
 Mas se o número buscado for maior que o meio do array, descarta a parte esquerda e busca denovo
 Faz isso recursivamente até que o número seja encontrado ou que não haja o número
+Retorna o índice do número buscado
 */
 
-function binarySearch(arr, target,) {
-  
+function binarySearch(arr, target) {
+  return search(arr, target, 0, arr.length - 1)
 }
 
 function search(arr, target, left, right) {
-  let half = (left + right) / 2
+  if (left > right) {
+    return -1
+  }
 
-  if (half) {
-    
+  let half = Math.floor((left + right) / 2)
+
+  if (arr[half] === target) {
+    return half
+  } else if (arr[half] > target) {
+    return search(arr, target, left, half - 1)
+  } else {
+    return search(arr, target, half + 1, right)
   }
 }
 
-binarySearch([1,3,4,6,8,11], 6)
+console.log(binarySearch([1,3,4,6,8,11], 6))
