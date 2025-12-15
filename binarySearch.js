05b9fc1fe0f265923e02a6678a34bcc1435a387a
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-12-15 17:58:12 -0300

    15/12/2025

---
+++ binarySearch.js
@@ -0,0 +1,20 @@
+/*
+Encontra o meio do array
+Se o número buscado for menor que o valor do meio do array, descarta a parte direita e busca denovo
+Mas se o número buscado for maior que o meio do array, descarta a parte esquerda e busca denovo
+Faz isso recursivamente até que o número seja encontrado ou que não haja o número
+*/
+
+function binarySearch(arr, target,) {
+  
+}
+
+function search(arr, target, left, right) {
+  let half = (left + right) / 2
+
+  if (half) {
+    
+  }
+}
+
+binarySearch([1,3,4,6,8,11], 6)
--- isValidParen.js
+++ isValidParen.js
@@ -1,13 +1,49 @@
 /*
-Percorrer a string
+Se a string contiver algo que não é um parênteses, dá erro
+
+Percorre a string
+Se for um parênteses assim ( incrementa o contador em um
+Se for um parênteses assim ) diminui o contador em um
+
+Verifica se em algum momento o contador ficou negativo
+
+Se o valor nunca ficou negativo e terminou em 0, retorna true
+Senão, retorna false
 */
 
 function isValid(str) {
   if (!/^[()]+$/.test(str)) {
     throw new Error("String inválida");
-  }
+  };
 
-      
+  let count = 0;
+  let containsMinus = false;
+  const arr = [];
+
+  for (const char of str){
+    if (char === "(") {
+      count++;
+      arr.push(count);
+    } else if(char === ")") {
+      count--;
+      arr.push(count);
+    } else {
+      throw new Error('Inválido');
+    };
+  } ;
+  
+  arr.forEach(element => {
+    if (element < 0) {
+      containsMinus = true;
+      return containsMinus
+    }
+  });
+
+  if(count === 0 && containsMinus === false) {
+    console.log(true)
+  } else {
+    console.log(false)
+  }
 }
 
-isValid('(()())');
+isValid('(()()))(');
