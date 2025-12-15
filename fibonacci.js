eb892147c828c26d5ba5e27e7864269f83ce1d1a
cahves1 <athoschaveslopesdealmeida@gmail.com>
2025-12-15 19:34:20 -0300

    15/12/2025

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+/* 
+Faz a equação de fibonacci n vezes
+A cada vez, atualiza o valor e insere no array
+*/
+
+function fibonacci(n) {
+  if (typeof n !== "number") {
+    return
+  }
+
+  const fibs = [];
+
+  while (x <= n) {
+    x++
+  }
+}
